aaf518e070f55ca07403af1ca6e52cdb317b5594
Japan Dave <davejapan2001@gmail.com>
2023-09-10 12:36:41 +0530

    Basic concepts of node

---
+++ first.js
@@ -0,0 +1,7 @@
+japan = {
+    name: "Japan",
+    no: 3,
+    developer: true
+}
+
+module.exports = japan;
---
+++ index.js
@@ -0,0 +1,11 @@
+const japan = require("./first");
+
+console.log("Hello", japan)
+
+console.log(exports, require, module, __filename, __dirname)
+
+// const http = require('http')
+
+// http.createServer((req, res)=>{
+
+// }).listen(5000)
